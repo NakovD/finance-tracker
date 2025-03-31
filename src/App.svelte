72488ca1c3e2c364f3e49e4aca782e2304da4af6
David Nakov <nakovdavid98@gmail.com>
2025-03-31 16:55:08 +0300

    initialize Vite + Svelte + TypeScript project with Tailwind CSS and basic app structure

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: false,
+      },
+    },
+  });
+  import Finance from "./features/finance/Finance.svelte";
+</script>
+
+<QueryClientProvider client={queryClient}>
+  <main>
+    <Finance />
+  </main>
+</QueryClientProvider>
+
+<!-- <style>
+</style> -->
