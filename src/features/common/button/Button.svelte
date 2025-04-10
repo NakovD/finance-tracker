40b4f2563a4d21320552142fc68a40506c28f9fb
David Nakov <nakovdavid98@gmail.com>
2025-04-10 19:42:43 +0300

    feat: add Button component for reusable button functionality

---
+++ src/features/common/button/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import type { HTMLButtonAttributes } from "svelte/elements";
+  import { cn } from "../../../infrastructure/utilities/twUtils";
+
+  const {
+    children,
+    class: _class,
+    ...rest
+  }: Omit<HTMLButtonAttributes, "children"> & { children: Snippet<[]> } =
+    $props();
+</script>
+
+<button {...rest} class={cn(_class, "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded")}>
+  {@render children()}
+</button>
