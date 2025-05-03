dab6a124ba7a4a615684043420099f149a68060f
David Nakov <nakovdavid98@gmail.com>
2025-05-03 11:24:46 +0300

    feat: update routing for MontlyFinance and add YearlyFinance component

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { Router, Route } from "svelte-routing";
   import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
   import MontlyFinance from "./features/finance/montlyFinance/MontlyFinance.svelte";
+  import YearlyFinance from "./features/finance/yearlyFinance/YearlyFinance.svelte";
 
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -16,8 +17,10 @@
 <QueryClientProvider client={queryClient}>
   <main>
     <Router>
-      <Route path="/montly-finance/:id" component={MontlyFinance} />
+      <Route path="/montly-finance/:id" let:params>
+        <MontlyFinance id="{params.id}" />
+    </Route>
+      <Route path="/yearly-finance/:id" component={YearlyFinance} />
     </Router>
-    <MontlyFinance />
   </main>
 </QueryClientProvider>
