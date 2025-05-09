2626baa5167d2c52314ecfd1df349fc1e20a872f
David Nakov <nakovdavid98@gmail.com>
2025-05-09 20:19:05 +0300

    feat: add AllYears route for displaying annual finance overview

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
   import MontlyFinance from "./features/finance/montlyFinance/MontlyFinance.svelte";
   import YearlyFinance from "./features/finance/yearlyFinance/YearlyFinance.svelte";
+  import AllYears from "./features/finance/overview/AllYears.svelte";
 
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -23,6 +24,7 @@
       <Route path="/yearly-finance/:id" let:params>
         <YearlyFinance id={params.id} /></Route
       >
+      <Route path="/"><AllYears /></Route>
     </Router>
   </main>
 </QueryClientProvider>
