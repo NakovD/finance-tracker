c6e9dbe3f1b9551cc794a2d8a7864a0a919d7e8f
David Nakov <nakovdavid98@gmail.com>
2025-05-20 11:09:22 +0300

    refactor: simplify InputFieldProps by removing unused properties and binding

--- src/features/common/form/Inputfield.svelte
+++ src/features/common/form/Inputfield.svelte
@@ -1,38 +1,19 @@
 <script lang="ts">
   import type {
-    FocusEventHandler,
     HTMLInputAttributes,
-    HTMLInputTypeAttribute,
   } from "svelte/elements";
   import { cn } from "../../../infrastructure/utilities/twUtils";
 
   type InputFieldProps = {
-    id: string;
-    type: HTMLInputTypeAttribute;
-    value: string;
     error?: string;
-    placeholder: string;
-    onBlur?: HTMLInputAttributes["onblur"];
-  };
+  } & HTMLInputAttributes;
 
-  let {
-    id,
-    type,
-    value = $bindable(""),
-    placeholder,
-    error,
-    onBlur,
-  }: InputFieldProps = $props();
+  const { class: className, error, ...other }: InputFieldProps = $props();
 </script>
 
 <input
-  {id}
-  {type}
-  {placeholder}
-  {value}
-  onblur={onBlur}
-  oninput={({ currentTarget }) => (value = currentTarget.value)}
   class={cn(
+    className,
     {
       "border-amber-200 focus:border-blue-500 focus:ring-blue-500":
         error === undefined,
@@ -40,4 +21,5 @@
     { "border-red-500": error !== undefined },
     "py-2.5 px-4 block w-full rounded-lg outline-none border-1 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
   )}
+  {...other}
 />
