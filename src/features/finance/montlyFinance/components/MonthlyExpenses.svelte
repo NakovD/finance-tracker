a9fbcb029e08e72d027feaa96b115974d7d34ded
David Nakov <nakovdavid98@gmail.com>
2025-05-26 09:18:43 +0300

    feat: add search functionality to filter expenses by name or description

--- src/features/finance/montlyFinance/components/MonthlyExpenses.svelte
+++ src/features/finance/montlyFinance/components/MonthlyExpenses.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Inputfield from "../../../common/form/Inputfield.svelte";
   import type { Expense } from "../models/expense";
   import type { ExpenseCategory as ExpenseCategoryType } from "../models/expenseCategory";
   import type { MontlyFinance } from "../models/montlyFinance";
@@ -11,16 +12,40 @@
 
   const groupedByCategories = Object.groupBy(expenses, (e) => e.category);
   const categories = Object.keys(groupedByCategories) as ExpenseCategoryType[];
+
+  let search = $state("");
+
+  let filteredExpensesCategories = $derived(
+    categories.filter(
+      (category) =>
+        groupedByCategories[category] !== undefined &&
+        groupedByCategories[category].some(
+          (expense) =>
+            expense.name.toLowerCase().includes(search.toLowerCase()) ||
+            expense.description?.toLowerCase().includes(search.toLowerCase())
+        )
+    )
+  );
 </script>
 
+<Inputfield
+  type="text"
+  value={search}
+  class="max-w-xs"
+  placeholder="Search for an expense in all categories"
+  oninput={({ currentTarget }) => (search = currentTarget.value)}
+/>
+<div class="mb-4"></div>
 <div class="flex flex-col gap-4">
-  {#each categories as category}
-    {#if groupedByCategories[category] !== undefined}
-      <ExpenseCategory
-        categoryName={category}
-        expenses={groupedByCategories[category]}
-        montlyFinance={monthFinance}
-      />
-    {/if}
-  {/each}
+  {#key filteredExpensesCategories}
+    {#each filteredExpensesCategories as category}
+      {#if groupedByCategories[category] !== undefined}
+        <ExpenseCategory
+          categoryName={category}
+          expenses={groupedByCategories[category]}
+          montlyFinance={monthFinance}
+        />
+      {/if}
+    {/each}
+  {/key}
 </div>
