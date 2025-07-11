daf29be46e3549de540ed25929e8689c3b5e5d18
David Nakov <nakovdavid98@gmail.com>
2025-07-11 20:07:42 +0300

    feat: implement toaster utility for enhanced notification handling

---
+++ src/features/common/toaster/toaster.ts
@@ -0,0 +1,7 @@
+import { toast } from "svelte-sonner";
+
+export const toaster = {
+  showSuccess: (args: Parameters<typeof toast.success>) => toast(...args),
+  showError: (args: Parameters<typeof toast.error>) => toast(...args),
+  showInfo: (args: Parameters<typeof toast.info>) => toast(...args),
+} as const;
