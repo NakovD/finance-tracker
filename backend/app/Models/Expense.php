13102151e170c328b689aeffb0d6244c4889c764
David Nakov <nakovdavid98@gmail.com>
2025-08-29 20:12:22 +0300

    feat: add relationship method for MontlyFinance in Expense model

--- backend/app/Models/Expense.php
+++ backend/app/Models/Expense.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Expense extends Model
 {
     //
+
+
+    public function MontlyFinance(): HasOne
+    {
+        return $this->hasOne(MontlyFinance::class);
+    }
 }
