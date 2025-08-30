c6f6c755a25726259f338a414048d4af139f163b
David Nakov <nakovdavid98@gmail.com>
2025-08-30 20:23:24 +0300

    feat: add expenses relationship method to MontlyFinance model

--- backend/app/Models/MontlyFinance.php
+++ backend/app/Models/MontlyFinance.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class MontlyFinance extends Model
 {
     //
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
 }
