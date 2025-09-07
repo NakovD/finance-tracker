62af198ec9843473453fe9e0524afce95af86125
David Nakov <nakovdavid98@gmail.com>
2025-09-07 12:45:26 +0300

    feat: add fillable attributes to Expense model

--- backend/app/Models/Expense.php
+++ backend/app/Models/Expense.php
@@ -9,6 +9,12 @@ class Expense extends Model
 {
     //
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['name', 'description', 'amount', 'date', 'category'];
 
     public function MontlyFinance(): HasOne
     {
