6d73e02522c589172a3d38d2119e40b388cf1ff3
David Nakov <nakovdavid98@gmail.com>
2025-09-13 10:01:15 +0300

    feat: implement Delete method in ExpenseService for expense removal functionality

--- backend/app/Services/ExpenseService.php
+++ backend/app/Services/ExpenseService.php
@@ -16,7 +16,6 @@ class ExpenseService
         return new MessageResult("Expense created", true, 201);
     }
 
-
     public function Update(array $data): MessageResult
     {
         $expense = Expense::where("id", $data["id"])
@@ -29,5 +28,20 @@ class ExpenseService
         $expense->update($data);
 
         return new MessageResult("Expense updated", true, 200);
+    }
+
+    public function Delete(int $expenseId): MessageResult
+    {
+        $expense = Expense::where("id", $expenseId)
+            ->first();
+
+        if (!$expense) {
+            return new MessageResult("Expense not found", false, 404);
+        }
+
+        $expense->delete();
+
+        return new MessageResult("Expense deleted", true, 200);
+
     }
 }
