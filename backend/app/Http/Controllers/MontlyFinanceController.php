6f84e655f863350057b7f543d5b108e53cd09757
David Nakov <nakovdavid98@gmail.com>
2025-10-22 16:36:01 +0300

    feat: implement MontlyFinanceController with CRUD operations

---
+++ backend/app/Http/Controllers/MontlyFinanceController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\MontlyFinanceRequest;
+use App\Models\MontlyFinance;
+use App\Services\MontlyFinanceService;
+use Illuminate\Http\Request;
+
+class MontlyFinanceController extends Controller
+{
+
+    public function __construct(private MontlyFinanceService $montlyFinanceService) {
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(MontlyFinanceRequest $request)
+    {
+        $validated = $request->validated();
+
+        $result = $this->montlyFinanceService->Create($validated);
+
+        return response($result->data, $result->status_code);  
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MontlyFinance $montlyFinance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MontlyFinance $montlyFinance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MontlyFinance $montlyFinance)
+    {
+        //
+    }
+}
