c7e56c28bade6728b0d7dcf13c6d5d666a16ef9a
David Nakov <nakovdavid98@gmail.com>
2025-12-10 19:46:35 +0200

    feat: enhance createQueryFacade with validation and error handling

--- frontend/src/infrastructure/api/createQueryFacade.ts
+++ frontend/src/infrastructure/api/createQueryFacade.ts
@@ -1,17 +1,51 @@
 import { createQuery, type CreateQueryOptions } from "@tanstack/svelte-query";
 import { httpInstance } from "./httpInstance";
-import type { HTTPError } from "ky";
+import { HTTPError } from "ky";
+import { object, string, type ZodObject } from "zod";
+import type { AppError } from "./models/AppError";
 
-type FacadeOptions<T extends object> = Omit<
-  CreateQueryOptions<T, HTTPError>,
+type FacadeOptions<T extends Record<string, any>> = Omit<
+  CreateQueryOptions<T, AppError>,
   "queryFn"
-> & { endpoint: string };
+> & { endpoint: string; validator: ZodObject<T> };
 
 export const createQueryFacade = <T extends object>({
   endpoint,
+  validator,
   ...rest
 }: FacadeOptions<T>) =>
-  createQuery<T, HTTPError>({
+  createQuery<T, AppError>({
     ...rest,
-    queryFn: () => httpInstance.get(endpoint).json<T>(),
+    queryFn: async () => {
+      try {
+        const result = await httpInstance.get(endpoint).json<T>();
+
+        const validation = validator.safeParse(result);
+
+        if (!validation.success) {
+          throw <AppError>{
+            type: "validation",
+            error: validation.error,
+          };
+        }
+
+        return result;
+      } catch (error) {
+        if (error instanceof HTTPError) {
+          throw <AppError>{
+            type: "error",
+            error: error,
+          };
+        }
+
+        if (typeof error === "object" && error && "type" in error) {
+          throw error as AppError;
+        }
+
+        throw <AppError>{
+          type: "unknown",
+          error: error,
+        };
+      }
+    },
   });
