fc10910e5f226966817132c20c652ff64c5c6560
David Nakov <nakovdavid98@gmail.com>
2025-12-11 20:43:24 +0200

    feat: implement GET method with validation and error handling

---
+++ frontend/src/infrastructure/api/methods.ts
@@ -0,0 +1,42 @@
+import { HTTPError } from "ky";
+import { httpInstance } from "./httpInstance";
+import type { AppError } from "./models/AppError";
+import type { ZodObject } from "zod";
+
+export const METHODS = {
+  GET: async <T extends Record<string, any>>(
+    endpoint: string,
+    validator: ZodObject<T>
+  ) => {
+    try {
+      const result = await httpInstance.get(endpoint).json<T>();
+
+      const validation = validator.safeParse(result);
+
+      if (!validation.success) {
+        throw <AppError>{
+          type: "validation",
+          error: validation.error,
+        };
+      }
+
+      return result;
+    } catch (error) {
+      if (error instanceof HTTPError) {
+        throw <AppError>{
+          type: "error",
+          error: error,
+        };
+      }
+
+      if (typeof error === "object" && error && "type" in error) {
+        throw error as AppError;
+      }
+
+      throw <AppError>{
+        type: "unknown",
+        error: error,
+      };
+    }
+  },
+} as const;
