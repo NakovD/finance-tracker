c57f219bd8d3e72f3074b753a4a88bacdfae36b1
David Nakov <nakovdavid98@gmail.com>
2025-12-15 20:44:09 +0200

    feat: refactor createQueryFacade to simplify query function and improve readability

--- frontend/src/infrastructure/api/createQueryFacade.ts
+++ frontend/src/infrastructure/api/createQueryFacade.ts
@@ -3,49 +3,19 @@ import { httpInstance } from "./httpInstance";
 import { HTTPError } from "ky";
 import { object, string, type ZodObject } from "zod";
 import type { AppError } from "./models/AppError";
+import { METHODS } from "./methods";
 
 type FacadeOptions<T extends Record<string, any>> = Omit<
   CreateQueryOptions<T, AppError>,
   "queryFn"
 > & { endpoint: string; validator: ZodObject<T> };
 
-export const createQueryFacade = <T extends object>({
+export const createQueryFacade = <T extends Record<string, any>>({
   endpoint,
   validator,
   ...rest
 }: FacadeOptions<T>) =>
   createQuery<T, AppError>({
     ...rest,
-    queryFn: async () => {
-      try {
-        const result = await httpInstance.get(endpoint).json<T>();
-
-        const validation = validator.safeParse(result);
-
-        if (!validation.success) {
-          throw <AppError>{
-            type: "validation",
-            error: validation.error,
-          };
-        }
-
-        return result;
-      } catch (error) {
-        if (error instanceof HTTPError) {
-          throw <AppError>{
-            type: "error",
-            error: error,
-          };
-        }
-
-        if (typeof error === "object" && error && "type" in error) {
-          throw error as AppError;
-        }
-
-        throw <AppError>{
-          type: "unknown",
-          error: error,
-        };
-      }
-    },
+    queryFn: () => METHODS.GET(endpoint, validator),
   });
