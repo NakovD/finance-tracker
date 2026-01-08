5782a971703a8f8cf3b0c6dcb993d32c51ba3214
David Nakov <nakovdavid98@gmail.com>
2026-01-08 17:25:47 +0200

    feat: add headers and credentials to httpInstance configuration

--- frontend/src/infrastructure/api/httpInstance.ts
+++ frontend/src/infrastructure/api/httpInstance.ts
@@ -3,4 +3,8 @@ import ky from "ky";
 export const httpInstance = ky.create({
   prefixUrl: import.meta.env.VITE_API_BASE_URL,
   retry: 0,
+  headers: new Headers({
+    accept: "application/json",
+  }),
+  credentials: "include",
 });
