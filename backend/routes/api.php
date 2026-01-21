90d5b3aae484e9f72ebc66e0029bab52e550c34e
David Nakov <nakovdavid98@gmail.com>
2026-01-21 13:23:57 +0200

    feat: transform user data into UserResource in user endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\ExpenseController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MonthlyFinanceController;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    return new UserResource($request->user());
 })->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
