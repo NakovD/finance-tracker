82cd8ed82242e5e5188be81ad22569b1bc940f66
David Nakov <nakovdavid98@gmail.com>
2026-02-28 19:35:05 +0200

    feat: refactor MonthlyFinance component to use createQueryFacade for data fetching

--- frontend/src/features/finance/monthlyFinance/MonthlyFinance.svelte
+++ frontend/src/features/finance/monthlyFinance/MonthlyFinance.svelte
@@ -1,26 +1,25 @@
 <script lang="ts">
-  import { createQuery } from "@tanstack/svelte-query";
-  import type { MonthlyFinance } from "./models/monthlyFinance";
   import AddExpenseForm from "./components/AddExpenseForm.svelte";
-  import { expenseTrackerDB } from "../../../infrastructure/db";
-  import { handleDbAction } from "../../../infrastructure/db/utilities/handleDbAction";
   import { sum } from "../../../infrastructure/utilities/reduceUtility";
   import MonthlyExpenses from "./components/MonthlyExpenses.svelte";
   import Loader from "../../common/loader/Loader.svelte";
+  import { createQueryFacade } from "../../../infrastructure/api/createQueryFacade";
+  import { endpoints } from "../../../infrastructure/api/endpoints/endpoints";
+  import { monthlyFinanceQueryValidator } from "./validators/monthlyFinanceValidator";
 
   let { id }: { id: string } = $props();
 
-  const query = createQuery<MonthlyFinance>({
+  const query = createQueryFacade({
     queryKey: ["monthly-finance", id],
-    queryFn: () =>
-      handleDbAction(() => expenseTrackerDB.getSingleById<MonthlyFinance>(id)),
+    endpoint: endpoints.monthlyFinances.getMonthlyFinance(id),
+    validator: monthlyFinanceQueryValidator,
   });
 </script>
 
 {#if $query.isLoading}
   <Loader />
 {:else if $query.isError}
-  <p>Error: {$query.error.message}</p>
+  <p>Error: {$query.error.error}</p>
 {:else if $query.isSuccess}
   <div class="max-w-2xl my-8 mx-auto">
     <h1>
