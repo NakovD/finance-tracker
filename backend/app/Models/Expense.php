c2df62e36d2c63ce5a5d18ddd349941149048ee5
David Nakov <nakovdavid98@gmail.com>
2026-03-04 18:36:27 +0200

    feat: remove unused Request import and add casts method for category in Expense model

--- backend/app/Models/Expense.php
+++ backend/app/Models/Expense.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Enums\ExpenseCategory;
 
 
 class Expense extends Model
@@ -19,6 +20,13 @@ class Expense extends Model
      * @var array<int, string>
      */
     protected $fillable = ['name', 'description', 'amount', 'date', 'category'];
+
+   protected function casts(): array
+    {
+        return [
+            'category' => ExpenseCategory::class,
+        ];
+    }
 
     public function MonthlyFinance(): BelongsTo
     {
