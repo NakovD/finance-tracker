ad9fb4f4fea50f860212927fcc59793b0f31a8a5
David Nakov <nakovdavid98@gmail.com>
2026-03-17 20:32:41 +0200

    feat: extend ExpenseRequest type to include monthly_finance_id

--- frontend/src/features/finance/monthlyFinance/models/expense.ts
+++ frontend/src/features/finance/monthlyFinance/models/expense.ts
@@ -2,3 +2,7 @@ import type { expenseValidator } from "../validators/expenseValidator";
 import type { infer as ZodInfer } from "zod";
 
 export type Expense = ZodInfer<typeof expenseValidator>;
+
+export type ExpenseRequest = Omit<Expense, "id"> & {
+  monthly_finance_id: number;
+};
