a50b0e1cd899c1d45462783aeaf7e6fa88286c35
David Nakov <nakovdavid98@gmail.com>
2026-04-07 09:53:28 +0300

    refactor: improve loader component structure and class handling

--- frontend/src/features/common/loader/Loader.svelte
+++ frontend/src/features/common/loader/Loader.svelte
@@ -1,6 +1,15 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import { cn } from "../../../infrastructure/utilities/twUtils";
 
-<div class="flex justify-center items-center min-h-screen bg-neutral-700">
+  const { class: _class }: { class?: string } = $props();
+</script>
+
+<div
+  class={cn(
+    "flex justify-center items-center min-h-screen bg-neutral-700",
+    _class,
+  )}
+>
   <div
     class="w-12 h-12 border-4 border-cyan-600 border-t-transparent rounded-full animate-spin"
   ></div>
